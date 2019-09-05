9ac12ff9dd1b09ee56fe490f42d5369cab6800b9
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-09-04 22:12:54 -0700

    Refactor deck methods specs

--- spec/active_recall/deck_methods_spec.rb
+++ spec/active_recall/deck_methods_spec.rb
@@ -1,22 +1,25 @@
-# -*- encoding: utf-8 -*-
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe ActiveRecall::DeckMethods do
-  before(:each) do
-    @word = Word.create!(:kanji => "日本語", :kana => "にほんご", :translation => "Japanese language")
-    @user = User.create!(:name => "Robert")
+  let(:word) do
+    Word.create!(
+      kanji: '日本語',
+      kana: 'にほんご',
+      translation: 'Japanese language'
+    )
   end
+  let(:user) { User.create!(name: 'Robert') }
 
-  context "Decks" do
-    describe "#words" do
-      it "should return an empty list when empty" do
-        @user.words.should == []
-      end
+  describe '#words' do
+    it 'should return an empty list when empty' do
+      expect(user.words).to eq([])
+    end
 
-      it "should allow access of word stats" do
-        @user.words << @word
-        @word.stats
-      end
+    it 'should allow access of word stats' do
+      user.words << word
+      expect(word.stats).to be
     end
   end
 end
