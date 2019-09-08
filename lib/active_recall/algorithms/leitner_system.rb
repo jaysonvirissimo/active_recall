14966a274b37c1325a0a6deb0821c68ae9d9d1d8
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-09-08 11:45:25 -0700

    Namespace algorithms

--- lib/active_recall/algorithms/leitner_system.rb
+++ lib/active_recall/algorithms/leitner_system.rb
@@ -1,44 +1,46 @@
 # frozen_string_literal: true
 
-class LeitnerSystem
-  DELAYS = [3, 7, 14, 30, 60, 120, 240].freeze
+module ActiveRecall
+  class LeitnerSystem
+    DELAYS = [3, 7, 14, 30, 60, 120, 240].freeze
 
-  def self.right(item, current_time: Time.current)
-    new(item, current_time: current_time).right
-  end
+    def self.right(item, current_time: Time.current)
+      new(item, current_time: current_time).right
+    end
 
-  def self.wrong(item, current_time: Time.current)
-    new(item, current_time: current_time).wrong
-  end
+    def self.wrong(item, current_time: Time.current)
+      new(item, current_time: current_time).wrong
+    end
 
-  def initialize(item, current_time: Time.current)
-    @item = item
-    @current_time = current_time
-  end
+    def initialize(item, current_time: Time.current)
+      @item = item
+      @current_time = current_time
+    end
 
-  def right
-    {
-      box: item.box + 1,
-      times_right: item.times_right + 1,
-      last_reviewed: current_time,
-      next_review: next_review
-    }
-  end
+    def right
+      {
+        box: item.box + 1,
+        times_right: item.times_right + 1,
+        last_reviewed: current_time,
+        next_review: next_review
+      }
+    end
 
-  def wrong
-    {
-      box: 0,
-      times_wrong: item.times_wrong + 1,
-      last_reviewed: current_time,
-      next_review: nil
-    }
-  end
+    def wrong
+      {
+        box: 0,
+        times_wrong: item.times_wrong + 1,
+        last_reviewed: current_time,
+        next_review: nil
+      }
+    end
 
-  private
+    private
 
-  attr_reader :item, :current_time
+    attr_reader :item, :current_time
 
-  def next_review
-    (current_time + DELAYS[[DELAYS.count, item.box + 1].min - 1].days)
+    def next_review
+      (current_time + DELAYS[[DELAYS.count, item.box + 1].min - 1].days)
+    end
   end
 end
