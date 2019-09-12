09c1033e80d6d76b01467d6826bb2517e53cd1de
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-09-11 21:43:23 -0700

    SRS algos are pure functions: decoupled from model

--- lib/active_recall/algorithms/leitner_system.rb
+++ lib/active_recall/algorithms/leitner_system.rb
@@ -4,23 +4,36 @@ module ActiveRecall
   class LeitnerSystem
     DELAYS = [3, 7, 14, 30, 60, 120, 240].freeze
 
-    def self.right(item, current_time: Time.current)
-      new(item, current_time: current_time).right
+    def self.right(box:, current_time: Time.current, times_right:, times_wrong:)
+      new(
+        box: box,
+        current_time: current_time,
+        times_right: times_right,
+        times_wrong: times_wrong
+      ).right
     end
 
-    def self.wrong(item, current_time: Time.current)
-      new(item, current_time: current_time).wrong
+    def self.wrong(box:, current_time: Time.current, times_right:, times_wrong:)
+      new(
+        box: box,
+        current_time: current_time,
+        times_right: times_right,
+        times_wrong: times_wrong
+      ).wrong
     end
 
-    def initialize(item, current_time: Time.current)
-      @item = item
+    def initialize(box:, current_time: Time.current, times_right:, times_wrong:)
+      @box = box
       @current_time = current_time
+      @times_right = times_right
+      @times_wrong = times_wrong
     end
 
     def right
       {
-        box: item.box + 1,
-        times_right: item.times_right + 1,
+        box: box + 1,
+        times_right: times_right + 1,
+        times_wrong: times_wrong,
         last_reviewed: current_time,
         next_review: next_review
       }
@@ -29,7 +42,8 @@ module ActiveRecall
     def wrong
       {
         box: 0,
-        times_wrong: item.times_wrong + 1,
+        times_right: times_right,
+        times_wrong: times_wrong + 1,
         last_reviewed: current_time,
         next_review: nil
       }
@@ -37,10 +51,10 @@ module ActiveRecall
 
     private
 
-    attr_reader :item, :current_time
+    attr_reader :box, :current_time, :times_right, :times_wrong
 
     def next_review
-      (current_time + DELAYS[[DELAYS.count, item.box + 1].min - 1].days)
+      (current_time + DELAYS[[DELAYS.count, box + 1].min - 1].days)
     end
   end
 end
