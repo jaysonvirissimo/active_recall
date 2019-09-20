0206f905984fc248ffde7e20ef80f19aad9d5c76
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-09-19 21:03:27 -0700

    Shared example group specs enforce algorithm API

---
+++ spec/active_recall/algorithms/algorithm_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+shared_examples_for 'spaced repetition algorithms' do
+  it { expect(described_class).to respond_to(:right) }
+  it { expect(described_class).to respond_to(:wrong) }
+
+  context 'when given API-respecting arguments' do
+    let(:arguments) { { box: 0, times_right: 0, times_wrong: 0 } }
+    let(:expected_keys) do
+      %i[box times_right times_wrong last_reviewed next_review]
+    end
+
+    describe '#right' do
+      let(:result) { described_class.new(arguments).right }
+
+      it 'returns attributes needed to update the ActiveRecord model' do
+        expect(result.keys).to include(*expected_keys)
+        expect(result[:box]).to be_kind_of(Integer)
+        expect(result[:times_right]).to be_kind_of(Integer)
+        expect(result[:times_wrong]).to be_kind_of(Integer)
+        expect(result[:last_reviewed]).to be_kind_of(Time)
+        expect(result[:next_review]).to be_kind_of(Time)
+      end
+    end
+
+    describe '#wrong' do
+      let(:result) { described_class.new(arguments).wrong }
+
+      it 'returns attributes needed to update the ActiveRecord model' do
+        expect(result.keys).to include(*expected_keys)
+        expect(result[:box]).to be_kind_of(Integer)
+        expect(result[:times_right]).to be_kind_of(Integer)
+        expect(result[:times_wrong]).to be_kind_of(Integer)
+        expect(result[:last_reviewed]).to be_kind_of(Time)
+      end
+    end
+  end
+end
