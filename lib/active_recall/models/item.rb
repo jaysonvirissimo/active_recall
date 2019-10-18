a3e441141ff3e1157f4eb67915cb926b647bcc30
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-10-17 20:54:57 -0700

    SRS algorithm is configurable at runtime (#9)

--- lib/active_recall/models/item.rb
+++ lib/active_recall/models/item.rb
@@ -3,10 +3,12 @@
 module ActiveRecall
   class Item < ActiveRecord::Base
     self.table_name = 'active_recall_items'
+
     belongs_to :deck
     belongs_to :source, polymorphic: true
-    scope :untested, -> { where(['box = ? and last_reviewed is null', 0]) }
+
     scope :failed, -> { where(['box = ? and last_reviewed is not null', 0]) }
+    scope :untested, -> { where(['box = ? and last_reviewed is null', 0]) }
 
     def self.expired(current_time: Time.current)
       where(['box > ? and next_review <= ?', 0, current_time])
@@ -17,14 +19,18 @@ module ActiveRecall
     end
 
     def right!
-      update!(LeitnerSystem.right(scoring_attributes))
+      update!(algorithm_class.right(scoring_attributes))
     end
 
     def wrong!
-      update!(LeitnerSystem.wrong(scoring_attributes))
+      update!(algorithm_class.wrong(scoring_attributes))
     end
 
     private
+
+    def algorithm_class
+      ActiveRecall.configuration.algorithm_class
+    end
 
     def scoring_attributes
       attributes.symbolize_keys.slice(:box, :times_right, :times_wrong)
