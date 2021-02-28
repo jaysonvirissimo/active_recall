e8c4ac030a43540e717844cac64b33c2966de45a
Jayson Virissimo <jayson.virissimo@asu.edu>
2021-02-28 15:57:25 -0700

    Refactor item methods
    Update items only once, rather than twice.

--- lib/active_recall/item_methods.rb
+++ lib/active_recall/item_methods.rb
@@ -3,15 +3,11 @@
 module ActiveRecall
   module ItemMethods
     def right_answer_for!(item)
-      i = deck.items.find_by(source_id: item.id)
-      i.right!
-      i.save!
+      deck.items.find_by(source_id: item.id).right!
     end
 
     def wrong_answer_for!(item)
-      i = deck.items.find_by(source_id: item.id)
-      i.wrong!
-      i.save!
+      deck.items.find_by(source_id: item.id).wrong!
     end
   end
 end
